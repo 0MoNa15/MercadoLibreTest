8aa44b73d11fd20c8b552e1da8af3e7d3d44bd61
Zorayda Gutiérrez Montes <zoraiida14@gmail.com>
2024-07-14 12:17:52 -0500

    Se agrega el cargando para la vista del detalle de producto

--- app/src/main/java/com/mona15dev/mercadolibretest/detail/view/ProductDetailScreen.kt
+++ app/src/main/java/com/mona15dev/mercadolibretest/detail/view/ProductDetailScreen.kt
@@ -1,12 +1,15 @@
 package com.mona15dev.mercadolibretest.detail.view
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -31,7 +34,12 @@ fun ProductDetailScreen(
     Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
         when {
             isLoading -> {
-                //CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    CircularProgressIndicator()
+                }
             }
             errorMessage != null -> {
                 EmptyDetailView(
