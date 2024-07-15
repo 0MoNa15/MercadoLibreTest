63b2b56a4c202843ce612a3ce805ef550fc8fd07
Zorayda Gutiérrez Montes <zoraiida14@gmail.com>
2024-07-15 10:05:52 -0500

    Se agregan disparadores IO para las consultas REST

--- data/src/main/java/com/mona15dev/data/product/detail/repository/ProductDetailRetrofitRepository.kt
+++ data/src/main/java/com/mona15dev/data/product/detail/repository/ProductDetailRetrofitRepository.kt
@@ -4,6 +4,8 @@ import com.mona15dev.data.product.api.ProductNetwork
 import com.mona15dev.data.product.detail.dto.ProductDetailDto
 import com.mona15dev.domain.product.detail.model.ProductCondition
 import com.mona15dev.domain.product.detail.model.ProductDetail
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import retrofit2.HttpException
 import javax.inject.Inject
 
@@ -11,21 +13,23 @@ class ProductDetailRetrofitRepository @Inject constructor(
     private val network: ProductNetwork
 ) {
     suspend fun getProductDetail(productId: String) : ProductDetail {
-        return try {
-            val response = network.apiProductDetail(productId)
-            mapProductDetailDtoToProductDetail(response)
-        } catch (e: HttpException) {
-            // Tempral crear clase personalizada de manejo de errores
-            when (e.code()) {
-                400 -> throw Exception("Bad Request")
-                401 -> throw Exception("Unauthorized")
-                404 -> throw Exception("Not Found")
-                500 -> throw Exception("Server Error")
-                else -> throw Exception("Unknown HTTP error")
-                //Temporal Para probar los estados, genera un 403 consultar una url incorrecta "https://api.mercadolibre.com/sites/MLA/items/MLA1680838032"
+        return withContext(Dispatchers.IO) {
+            try {
+                val response = network.apiProductDetail(productId)
+                mapProductDetailDtoToProductDetail(response)
+            } catch (e: HttpException) {
+                // Tempral crear clase personalizada de manejo de errores
+                when (e.code()) {
+                    400 -> throw Exception("Bad Request")
+                    401 -> throw Exception("Unauthorized")
+                    404 -> throw Exception("Not Found")
+                    500 -> throw Exception("Server Error")
+                    else -> throw Exception("Unknown HTTP error")
+                    //Temporal Para probar los estados, genera un 403 consultar una url incorrecta "https://api.mercadolibre.com/sites/MLA/items/MLA1680838032"
+                }
+            } catch (e: Exception) {
+                throw Exception("Network error: ${e.message}")
             }
-        } catch (e: Exception) {
-            throw Exception("Network error: ${e.message}")
         }
     }
 
--- data/src/main/java/com/mona15dev/data/product/list/repository/ProductListRetrofitRepository.kt
+++ data/src/main/java/com/mona15dev/data/product/list/repository/ProductListRetrofitRepository.kt
@@ -3,6 +3,8 @@ package com.mona15dev.data.product.list.repository
 import com.mona15dev.data.product.api.ProductNetwork
 import com.mona15dev.data.product.list.dto.ProductDto
 import com.mona15dev.domain.product.list.model.Product
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import retrofit2.HttpException
 import javax.inject.Inject
 
@@ -11,20 +13,22 @@ class ProductListRetrofitRepository @Inject constructor(
 ) {
 
     suspend fun getProductsByNameRetrofit(query: String) : List<Product> {
-        return try {
-            val response = network.apiSearchProducts(query)
-            response.listProducts.map { mapProductDtoToProduct(it) }
-        } catch (e: HttpException) {
-            //Temporal crear clase personalizada de manejo de errores
-            when (e.code()) {
-                400 -> throw Exception("Bad Request")
-                401 -> throw Exception("Unauthorized")
-                404 -> throw Exception("Not Found")
-                500 -> throw Exception("Server Error")
-                else -> throw Exception("Unknown HTTP error")
+        return withContext(Dispatchers.IO) {
+            try {
+                val response = network.apiSearchProducts(query)
+                response.listProducts.map { mapProductDtoToProduct(it) }
+            } catch (e: HttpException) {
+                //Temporal crear clase personalizada de manejo de errores
+                when (e.code()) {
+                    400 -> throw Exception("Bad Request")
+                    401 -> throw Exception("Unauthorized")
+                    404 -> throw Exception("Not Found")
+                    500 -> throw Exception("Server Error")
+                    else -> throw Exception("Unknown HTTP error")
+                }
+            } catch (e: Exception) {
+                throw Exception("Network error: ${e.message}")
             }
-        } catch (e: Exception) {
-            throw Exception("Network error: ${e.message}")
         }
     }
 }
