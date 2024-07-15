927a84cf3cefdd2ec304fb71134aaeb9793cb491
Zorayda Gutiérrez Montes <zoraiida14@gmail.com>
2024-07-15 14:29:26 -0500

    Mejora responsiva de la vista de lista vacia

--- app/src/main/java/com/mona15dev/mercadolibretest/list/view/state/EmptyListView.kt
+++ app/src/main/java/com/mona15dev/mercadolibretest/list/view/state/EmptyListView.kt
@@ -2,11 +2,11 @@ package com.mona15dev.mercadolibretest.list.view.state
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -18,25 +18,38 @@ import com.mona15dev.mercadolibretest.R
 
 @Composable
 fun EmptyListView() {
-    Column(
+    BoxWithConstraints(
         modifier = Modifier
-            .fillMaxWidth()
-            .fillMaxHeight()
-            .padding(dimensionResource(id = R.dimen.padding_double)),
-        verticalArrangement = Arrangement.Center
+            .fillMaxSize()
+            .padding(dimensionResource(id = R.dimen.padding_double))
     ) {
-        Image(
-            painter = painterResource(id = R.drawable.ic_empty_list),
-            contentDescription = null,
-            modifier = Modifier
-                .fillMaxWidth()
-                .aspectRatio(1f)
-        )
+        val maxHeight = maxHeight
+        val maxWidth = maxWidth
 
-        val emptyListText = stringResource(R.string.view_search_products)
-        Text(
-            text = emptyListText,
-            modifier = Modifier.align(Alignment.CenterHorizontally)
-        )
+        val imageSize = if (maxHeight < maxWidth) {
+            maxHeight * 0.5f
+        } else {
+            maxWidth * 0.5f
+        }
+
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_empty_list),
+                contentDescription = null,
+                modifier = Modifier.size(imageSize)
+            )
+
+            val emptyListText = stringResource(R.string.view_search_products)
+            Text(
+                text = emptyListText,
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .padding(top = dimensionResource(id = R.dimen.padding))
+            )
+        }
     }
 }
