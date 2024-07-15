3d61d353da21101737f6f5db31cfc55c8c86c8a8
Zorayda Gutiérrez Montes <zoraiida14@gmail.com>
2024-07-15 10:36:57 -0500

    Se agregan funciones de extensión para facilitar el mapeo entre tipos de modelos

--- data/src/main/java/com/mona15dev/data/product/list/database/entity/ProductEntity.kt
+++ data/src/main/java/com/mona15dev/data/product/list/database/entity/ProductEntity.kt
@@ -2,6 +2,7 @@ package com.mona15dev.data.product.list.database.entity
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.mona15dev.domain.product.list.model.Product
 
 @Entity
 data class ProductEntity (
@@ -11,4 +12,20 @@ data class ProductEntity (
     val price: Double,
     val thumbnail: String,
     val condition: String
+)
+
+fun ProductEntity.mapperToModel() = Product(
+    id = id,
+    title = title,
+    price = price,
+    thumbnail = thumbnail,
+    condition = condition
+)
+
+fun Product.mapperToEntity() = ProductEntity(
+    id = id,
+    title = title,
+    price = price,
+    thumbnail = thumbnail,
+    condition = condition
 )
